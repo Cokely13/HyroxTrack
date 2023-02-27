f7d9d4b944652c5eeff68bfa248f910f6def3366
Ryan Cokely <ryan.cokely@gmail.com>
2023-02-27 02:33:21 -0600

    added countdown clock'

---
+++ client/components/TargetDate.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CountdownTimer from './CountdownTimer';
+
+
+export default function TargetDate() {
+  const THREE_DAYS_IN_MS = new Date('March 18, 2023 09:00:00').getTime()
+  const NOW_IN_MS = new Date().getTime();
+
+  const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
+
+  return (
+    <div>
+      <h1>Countdown To Hyrox!</h1>
+      <CountdownTimer targetDate={THREE_DAYS_IN_MS} />
+    </div>
+  );
+}
