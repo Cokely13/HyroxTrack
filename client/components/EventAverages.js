707aa73e471a4c5e7a4c4b95e1ba3d71aa559f90
Ryan Cokely <ryan.cokely@gmail.com>
2023-06-01 16:02:42 -0400

    color coded

--- client/components/EventAverages.js
+++ client/components/EventAverages.js
@@ -1,55 +1,76 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import { useEffect, useState } from 'react'
-import { fetchEvents } from '../store/allEventsStore'
-import {fetchSingleUser} from '../store/singleUserStore'
-import { createResult } from '../store/allResultsStore'
+
+
+
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchEvents } from '../store/allEventsStore';
+import { fetchSingleUser } from '../store/singleUserStore';
 import { fetchResults } from '../store/allResultsStore';
 
 const EventAverages = () => {
-  const dispatch = useDispatch()
-  const events = useSelector((state) => state.allEvents )
-  const results = useSelector((state) => state.allResults )
-  const {id} = useSelector((state) => state.auth )
-  const user = useSelector((state) => state.singleUser )
-  const [adding, setAdding] = useState()
-  const [time, setTime] = useState()
-  const [eventName, setEventName] = useState()
-  const [addResult, setAddResult] = useState({})
-  useEffect(() => {
-    dispatch(fetchEvents())
-  }, [])
+  const dispatch = useDispatch();
+  const events = useSelector((state) => state.allEvents);
+  const results = useSelector((state) => state.allResults);
+  const { id } = useSelector((state) => state.auth);
+  const user = useSelector((state) => state.singleUser);
 
   useEffect(() => {
-    dispatch(fetchResults())
-  }, [])
-  useEffect(() => {
-    dispatch(fetchSingleUser(id))
-    // Safe to add dispatch to the dependencies array
-  }, [])
+    dispatch(fetchEvents());
+    dispatch(fetchResults());
+    dispatch(fetchSingleUser(id));
+  }, [dispatch, id]);
 
+  const userResults = results.filter((result) => result.userId === id);
 
-  const userResults = results.filter((result) => result.userId ==id)
+  const getAverageTime = (eventId) => {
+    const eventResults = userResults.filter((result) => result.eventId === eventId);
+    if (eventResults.length > 0) {
+      const totalTimes = eventResults.reduce((sum, result) => sum + parseInt(result.time), 0);
+      const average = totalTimes / eventResults.length;
+      return average.toFixed(2); // Limit decimal places to 2
+    }
+    return null;
+  };
 
+  const getDivStyle = (average, targetTime) => {
+    if (average === null) {
+      return { backgroundColor: 'yellow' };
+    } else if (average < targetTime) {
+      return { backgroundColor: 'green' };
+    } else {
+      return { backgroundColor: 'red', animation: 'flash 1s infinite' };
+    }
+  };
 
   return (
-
-
-<div className="container-fluid bg-3 text-center">
-          <div className="row align-items-stretch">
-          {events.length ? events.map((zone) => {
-        return (
+    <div className="container-fluid bg-3 text-center">
+      <div className="row align-items-stretch">
+        {events.length ? (
+          events.map((zone) => (
             <div className="col-sm-3 mx-auto mb-4 d-flex" key={zone.id}>
-                          <div className='col'>
-                        <h1>{zone.name}</h1>
-                        <h1>{zone.targetTime.slice(0,5)}</h1>
-                        {userResults.length ? userResults.filter((result)=> result.eventId == zone.id).map((zone) => {
-        return (
-          <h1>{zone.time}</h1>)}) : <div></div>}
-            </div></div>)}) :<div></div>}
-          </div>
-        </div>
+              <div className="col" style={getDivStyle(getAverageTime(zone.id), zone.targetTime)}>
+                <h1>{zone.name}</h1>
+                <h1>{zone.targetTime.slice(0, 5)}</h1>
+                {userResults.length ? (
+                  <React.Fragment>
+                    <h1>Average: {getAverageTime(zone.id)}</h1>
+                    {userResults
+                      .filter((result) => result.eventId === zone.id)
+                      .map((result) => (
+                        <h1 key={result.id}>{result.time}</h1>
+                      ))}
+                  </React.Fragment>
+                ) : (
+                  <div></div>
+                )}
+              </div>
+            </div>
+          ))
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </div>
   );
 };
 
