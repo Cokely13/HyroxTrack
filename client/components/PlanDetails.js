162766f9adb64c4995d923dda3f0be45f61c5dfd
Ryan Cokely <ryan.cokely@gmail.com>
2023-12-06 16:13:50 -0500

    added workout details

--- client/components/PlanDetails.js
+++ client/components/PlanDetails.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+import { fetchWorkout } from '../store/singleWorkoutStore';
 
-function PlanDetails({ plan }) {
+function PlanDetails() {
+  const dispatch = useDispatch();
+  const workout = useSelector((state) => state.singleWorkout);
 
-  console.log("plan", plan)
+  const { planId } = useParams();
+
+  useEffect(() => {
+    dispatch(fetchWorkout(planId));
+  }, [dispatch, planId]);
+
+  console.log("plan", workout)
   return (
     <div>
       <h2>Plan Details</h2>
-      <p>{plan}</p>
+      <p>{workout.name}</p>
+      <p>{workout.description}</p>
     </div>
   );
 }
