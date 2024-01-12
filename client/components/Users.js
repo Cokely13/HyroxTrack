18203e22ce95c8fe357959a7bf6550cdb20c0c23
Ryan Cokely <ryan.cokely@gmail.com>
2024-01-12 15:17:58 -0500

    updated format on users

--- client/components/Users.js
+++ client/components/Users.js
@@ -9,7 +9,6 @@ function Users() {
   const dispatch = useDispatch()
   const users = useSelector((state) => state.allUsers)
 
-  console.log("users", users)
   useEffect(() => {
     dispatch(fetchUsers())
   }, [])
@@ -17,25 +16,17 @@ function Users() {
 
 
 return (
-  <div className="text-center">
-    <h1 className="profile rounded text-center add" style={{ marginBottom: "15px", marginTop: "15px", marginLeft: "auto", marginRight: "auto", width: "35%" }}>
+  <div >
+    <h1 className="profile rounded text-center add">
       <b>Users</b>
     </h1>
-    <div className="users-container" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '20px' }}>
+    <div className="users-container">
     {users.length > 0 ? (
       users.map((user) => (
-        <div key={user.id} className="profile rounded text-center add " style={{ marginBottom: "15px", marginTop: "25px", marginLeft: "auto", marginRight: "auto", width: "15%" }}>
+        <div key={user.id} className="profile rounded text-center add ">
         {user.image && (
-           <div style={{
-            width: '100px',
-            height: '100px',
-            borderRadius: '50%',
-            margin: '10px auto 10px',
+           <div className="profile-image" style={{
             backgroundImage: `url(${user.image})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-            border: '3px solid black'
           }}> </div>
         )}
         <div style={{ marginTop: "15%"}} >
@@ -55,31 +46,6 @@ return (
 
 
 
-// return (
-//   <div className="text-center">
-//     <h1 className="profile rounded text-center add" style={{ marginBottom: "15px", marginTop: "15px", marginLeft: "auto", marginRight: "auto", width: "35%" }}>
-//       <b>Users</b>
-//     </h1>
-//     <div className="users-container" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '20px' }}>
-//       {users.length > 0 ? (
-//         users.map((user) => (
-//           <div key={user.id} className="user-profile" style={{ width: 'calc(25% - 20px)', textAlign: 'center' }}>
-//             {user.image && (
-//               <div className="profile-image-container" style={{ marginBottom: "15px" }}>
-//                 <img src={`http://localhost:8080/${user.image}`} alt={`${user.userName}'s profile`} style={{ maxWidth: '100%', height: 'auto', borderRadius: "50%" }} />
-//               </div>
-//             )}
-//             <Link to={`/users/${user.id}`}>
-//               <h2>{user.userName}</h2>
-//             </Link>
-//           </div>
-//         ))
-//       ) : (
-//         <div>No Users</div>
-//       )}
-//     </div>
-//   </div>
-// );
-// }
+
 
 export default Users
