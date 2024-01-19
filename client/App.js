1f8fb888110fe4809772d91b79d3e9e539c9d46a
Ryan Cokely <ryan.cokely@gmail.com>
2024-01-19 14:54:15 -0500

    loginpage

--- client/App.js
+++ client/App.js
@@ -1,21 +1,53 @@
+// import React from 'react'
+
+// import Navbar from './components/Navbar'
+// import Routes from './Routes'
+// import BottomBar from './components/BottomBar';
+// import CurtainMenu from './components/CurtainMenu';
+
+// const App = () => {
+//   return (
+//     <div>
+//       {/* <Navbar /> */}
+//       <CurtainMenu/>
+//       <div className="main-content">
+//       <Routes/>
+//       </div>
+//       <BottomBar/>
+//     </div>
+//   )
+// }
+
+// export default App
+
 import React from 'react'
+import { connect } from 'react-redux'
 
 import Navbar from './components/Navbar'
 import Routes from './Routes'
 import BottomBar from './components/BottomBar';
 import CurtainMenu from './components/CurtainMenu';
 
-const App = () => {
+const App = ({ isLoggedIn }) => {
   return (
     <div>
       {/* <Navbar /> */}
-      <CurtainMenu/>
+      {isLoggedIn && <CurtainMenu/>}
       <div className="main-content">
-      <Routes/>
+        <Routes/>
       </div>
-      <BottomBar/>
+      {isLoggedIn && <BottomBar/>}
     </div>
   )
 }
 
-export default App
+/**
+ * CONTAINER
+ */
+const mapState = state => {
+  return {
+    isLoggedIn: !!state.auth.id
+  }
+}
+
+export default connect(mapState)(App)
--- client/components/BottomBar.js
+++ client/components/BottomBar.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const BottomBar = () => {
+import {logout} from '../store'
+
+const BottomBar = ({ handleClick }) => {
+  // const dispatch = useDispatch();
   return (
     <div className="bottom-bar">
       <Link to="/home" className="bottom"><span className='fas fa-home'></span>Home</Link>
       <Link to="/profile" className="bottom"><span className='	fas fa-user-alt'></span>Profile</Link>
       <Link to="/results" className="bottom"><span className='fas fa-bars'></span>Results</Link>
       <Link  to="/mychallenges" className="bottom"><span className='	far fa-handshake'></span>Challenges</Link>
-      <Link to="/logout" className="bottom"><span className='	fas fa-door-open'></span>Logout</Link>
+      {/* <Link to="/login" className="bottom"><span className='	fas fa-door-open'></span>Logout</Link> */}
+      <a href="#" onClick={handleClick} className="bottom"><span className='fas fa-door-open'></span>Logout</a>
     </div>
   );
 };
 
-export default BottomBar;
+
+const mapDispatch = dispatch => {
+  return {
+    handleClick() {
+      dispatch(logout());
+    }
+  };
+};
+
+export default connect(null, mapDispatch)(BottomBar);
+
