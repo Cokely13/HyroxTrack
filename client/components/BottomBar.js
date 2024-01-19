c116704b483d6b9e6c99d9cd5c15653ee8009aeb
Ryan Cokely <ryan.cokely@gmail.com>
2024-01-19 13:46:25 -0500

    bottombar icons

--- client/components/BottomBar.js
+++ client/components/BottomBar.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 const BottomBar = () => {
   return (
     <div className="bottom-bar">
-      <Link to="/home" className="bottom">Home</Link>
-      <Link to="/profile" className="bottom">Profile</Link>
-      <Link to="/results" className="bottom">Results</Link>
-      <Link  to="/mychallenges" className="bottom"><span className='fas fa-plus-square'>Challenges</span></Link>
-      <Link to="/logout" className="bottom">Logout</Link>
+      <Link to="/home" className="bottom"><span className='fas fa-home'></span>Home</Link>
+      <Link to="/profile" className="bottom"><span className='	fas fa-user-alt'></span>Profile</Link>
+      <Link to="/results" className="bottom"><span className='fas fa-bars'></span>Results</Link>
+      <Link  to="/mychallenges" className="bottom"><span className='	far fa-handshake'></span>Challenges</Link>
+      <Link to="/logout" className="bottom"><span className='	fas fa-door-open'></span>Logout</Link>
     </div>
   );
 };
