ceaf6f7bd88f7311c4407aab3888e61c956f4368
Ryan Cokely <ryan.cokely@gmail.com>
2024-01-29 09:44:47 -0500

    error messages in login

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,15 +1,16 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { authenticate } from '../store';
 import { Link } from 'react-router-dom';
-import backgroundImage from '../Images/us.jpg'
+import { useSelector, useDispatch } from 'react-redux'
 import pic from '../../uploads/HyroxPic.jpg'
-// uploads/default.jpg
-// /Users/RCokely/HyroxTrack/uploads/default.jpg
+import { fetchUsers } from '../store/allUsersStore';
 
 const AuthForm = (props) => {
   const { name, displayName, handleSubmit, error } = props;
+  const allUsers = useSelector((state) => state.allUsers);
+  const dispatch = useDispatch()
 
   // State for confirm password and error message
   const [confirmPassword, setConfirmPassword] = useState('');
@@ -18,6 +19,11 @@ const AuthForm = (props) => {
   const handleConfirmPasswordChange = (event) => {
     setConfirmPassword(event.target.value);
   };
+
+
+  useEffect(() => {
+    dispatch(fetchUsers())
+  }, [])
 
   const enhancedHandleSubmit = (evt) => {
     evt.preventDefault();
@@ -31,18 +37,16 @@ const AuthForm = (props) => {
       return; // Don't dispatch if passwords don't match
     }
 
+
+    if (displayName === 'Login' && password !== confirmPassword) {
+      setPasswordError('Incorrect Password');
+      return;
+    }
     // If passwords match or it's the login form, proceed with the form submission
     setPasswordError(''); // Clear any previous error messages
     handleSubmit(evt, userName, password, formName);
   };
 
-  // const backgroundStyle = {
-  //   backgroundImage: `url(${pic})`,
-  //   backgroundSize: 'cover', // Cover the entire space of the element
-  //   backgroundPosition: 'center', // Center the image
-  //   backgroundRepeat: 'no-repeat', // Do not repeat the image
-  //   // Optionally, set other styles such as height, etc.
-  // };
 
   const backgroundStyle = {
     position: 'fixed', // Fixed position to cover the whole screen
@@ -57,81 +61,7 @@ const AuthForm = (props) => {
     zIndex: -1, // Ensure it's behind other content
   };
 
-//   return (
-//     <div style={backgroundStyle} >
 
-//     <div className="auth-container">
-//       <h1 className="profile rounded text-center add" style={{ marginBottom: '15px', marginTop: '25%', marginLeft: 'auto', marginRight: 'auto', width: '35%', backgroundColor: "white" }}>HyroxTrack</h1>
-//      <div style={{backgroundColor: "white", width: '65%', border: "3px solid darkgray", borderRadius: "10px"}}>
-//       <h1 className="profile rounded text-center add" >{displayName}</h1>
-//       <div className='frontpage'>
-//       <form onSubmit={enhancedHandleSubmit} name={name}>
-//         {displayName === "Login" ? (
-//           <div className="form-group">
-//             <label htmlFor="userName">
-//               <b>Username</b>
-//             </label>
-//             <input className="form-control" name="userName" type="text" />
-//           </div>
-//         ) : (
-//           <div className="form-group" >
-//             <label htmlFor="userName">
-//               <b>Create Username</b>
-//             </label>
-//             <input className="form-control" name="userName" type="text" />
-//           </div>
-//         )}
-//         {displayName === "Login" ? (
-//           <div className="form-group">
-//             <label htmlFor="password">
-//               <b>Password</b>
-//             </label>
-//             <input className="form-control" name="password" type="password" />
-//           </div>
-//         ) : (
-//           <div>
-//             <div className="form-group">
-//               <label htmlFor="password">
-//                 <b>Create Password</b>
-//               </label>
-//               <input className="form-control" name="password" type="password" />
-//             </div>
-//             <div className="form-group">
-//               <label htmlFor="confirmPassword">
-//                 <b>Confirm Password</b>
-//               </label>
-//               <input
-//                 className="form-control"
-//                 name="confirmPassword"
-//                 type="password"
-//                 value={confirmPassword}
-//                 onChange={handleConfirmPasswordChange}
-//               />
-//             </div>
-//           </div>
-//         )}
-//         <div>
-//           <button className="btn btn-primary" type="submit">
-//             {displayName}
-//           </button>
-//         </div>
-//         {passwordError && <div className="error-message">{passwordError}</div>}
-//         {error && error.response && <div className="error-message">{error.response.data}</div>}
-//       </form>
-//           {/* <div  style={{display: "flex", justifyContent: "center"}}> */}
-//       {displayName === "Login" ? (
-//         <Link style={{marginLeft: "auto", marginRight: "auto"}} to="/signup">Sign Up</Link>
-//       ) : (
-//         <Link style={{marginLeft: "auto", marginRight: "auto"}}  to="/login">Login</Link>
-//       )}
-//       {/* </div> */}
-//       </div>
-//        </div>
-
-//     </div>
-//     </div>
-//   );
-// };
 
 return (
   <div style={backgroundStyle}>
@@ -139,16 +69,19 @@ return (
       <h1 className="header">HyroxTrack</h1>
       <div className="frontPage">
         <h1><b>{displayName}</b></h1>
+        {passwordError && <div className="errorMessage">{passwordError}</div>}
+        {/* {passwordError && <div className="errorMessage">{passwordError}</div>} */}
+        {/* <form onSubmit={enhancedHandleSubmit} name={name} className="auth-form"></form> */}
         <form onSubmit={enhancedHandleSubmit} name={name} className="auth-form">
           <div className="input-group">
-            <label htmlFor="userName">
+            <label htmlFor="userName" style={{marginRight:"10px"}}>
               {displayName === "Login" ? "Username:" : "Create Username:"}
             </label>
             <input className="form-control" name="userName" type="text" />
           </div>
 
           <div className="input-group">
-            <label htmlFor="password">
+            <label htmlFor="password" style={{marginRight:"10px"}} >
               {displayName === "Login" ? "Password:": "Create Password:"}
             </label>
             <input className="form-control" name="password" type="password" />
@@ -156,7 +89,7 @@ return (
 
           {displayName === "Sign Up" && (
             <div className="input-group">
-              <label htmlFor="confirmPassword">Confirm Password: </label>
+              <label htmlFor="confirmPassword" style={{marginRight:"10px"}}>Confirm Password: </label>
               <input
                 className="form-control"
                 name="confirmPassword"
